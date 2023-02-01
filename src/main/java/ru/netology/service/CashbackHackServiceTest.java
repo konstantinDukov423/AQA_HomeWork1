46a4bd74934950796006e13509615f9ad1fff426
Kosta <dukov423@mail.ru>
2023-02-01 16:24:07 +0500

    add tests on TestNG

--- src/main/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/main/java/ru/netology/service/CashbackHackServiceTest.java
@@ -11,7 +11,7 @@ public class CashbackHackServiceTest {
 
         int expected = 1;
         int actual = service.remain(999);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -20,7 +20,7 @@ public class CashbackHackServiceTest {
 
         int expected = 0;
         int actual = service.remain(1000);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 
     @Test
@@ -29,6 +29,6 @@ public class CashbackHackServiceTest {
 
         int expected = 999;
         int actual = service.remain(1001);
-        assertEquals(expected, actual);
+        assertEquals(actual, expected);
     }
 }
